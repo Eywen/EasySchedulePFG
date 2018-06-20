5ea53ab4dfbc37b92e5f913971a02176a4aad886
Blanca Castaño <blk20100@gmail.com>
2018-06-20 17:32:20 +0200

    Funcionando OK: eliminar asignacion, crear asignación manual, editar asignación.

--- src/main/java/com/pfg/easyschedule/web/rest/AsignaturaProfesorResource.java
+++ src/main/java/com/pfg/easyschedule/web/rest/AsignaturaProfesorResource.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.net.URISyntaxException;
 import java.util.*;
 
@@ -33,35 +34,6 @@ public class AsignaturaProfesorResource {
         this.asignaturaRepository = asignaturaRepository;
     }
 
-
-    /**
-     * POST  /subjectsprofesors : to assign one subject to teacher.
-     *
-     * @param ,  the id_prof,id_asig to assign a subject to teacher
-     * @return the ResponseEntity with status 201 (Created) and with body the new asignatura, or with status 400 (Bad Request) if the asignatura has already an ID
-     * @throws URISyntaxException if the Location URI syntax is incorrect
-     */
-    @PostMapping("/asignaturaprofesors")
-     public ResponseEntity<Profesor> createAsignaturaProfesor(@RequestBody Map<String, String> json) throws URISyntaxException {
-        log.debug("REST request to save AsignaturaProfesor profesor : {}", json.get("id_asignatura"));
-        log.debug("REST request to save AsignaturaProfesor asignatura  : {}", json.get("id_profesor"));
-        String idProfesor = json.get("id_profesor");
-        String idAsignatura = json.get("id_asignatura");
-        Long id_prof= Long.parseLong(idProfesor, 10);
-        Long  id_asig = Long.parseLong(idAsignatura, 10);;
-
-        Profesor profesor = profesorRepository.findOne(id_prof);
-        Asignatura asignatura =asignaturaRepository.findOne(id_asig);
-        log.debug("REST request to save AsignaturaProfesor asignatura  : {} prof:{}", profesor , asignatura);
-        profesor.getAsignaturas().add(asignatura);
-        Profesor result = profesorRepository.save(profesor);
-        return Optional.ofNullable(result)
-            .map(myresult -> new ResponseEntity<>(
-                myresult,
-                HttpStatus.OK))
-            .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
-    }
-
     /**
     *DELETE  /asignaturaprofesors/delete the  asignaturaProfesors.
      * @return the ResponseEntity with status 200 (OK)
@@ -80,4 +52,6 @@ public class AsignaturaProfesorResource {
         Profesor result = profesorRepository.save(profesor);
         return ResponseEntity.ok().headers(HeaderUtil.createEntityDeletionAlert(ENTITY_NAME, asignatura.getId().toString())).build();
     }
+
+
 }
